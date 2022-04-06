cedaac4b7bb8a716331c6568f38a7a415ac68844
BBrandford11 <brandfordbryce@gmail.com>
2022-04-06 17:37:46 -0600

    ads count to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_pass']
   def show
+    @category = Category.count
+    @product = Product.count
   end
 end
