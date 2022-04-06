67f89280620dfdafabf6cef904be270875b8171b
BBrandford11 <brandfordbryce@gmail.com>
2022-04-06 16:42:15 -0600

    adds password and admin longin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_pass']
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_pass']
   def index
     @products = Product.order(id: :desc).all
   end
