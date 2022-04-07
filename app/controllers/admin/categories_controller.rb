a39507c6a2c49dc91f07d403bfe59c70b76489c5
BBrandford11 <brandfordbryce@gmail.com>
2022-04-07 17:59:17 -0600

    adds admin/catagories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,21 +2,23 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_pass']
 
   def index
-    @categorie = Category.all
+    @categories = Category.all
   end
 
   def new
-    @categorie = Category.new
+    @category = Category.new
   end
 
   def create
-    @categorie = Category.new(category_params)
+    @category = Category.new(@category_params)
 
-    if @categorie.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'Categorie created!'
     else
       render :new
     end
   end
+
+  
 
 end
